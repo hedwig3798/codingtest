4f5d0e646c4fa07286c48c827224a7f6904e323b
hedwig3798 <71327618+hedwig3798@users.noreply.github.com>
2022-12-14 16:54:42 +0900

    Create solution.cpp

---
+++ programers/점 찍기/solution.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include<cmath>
+
+using namespace std;
+
+long long solution(int k, int d) {
+    long long answer = 0;
+    long long a = 0;
+    double hight = 0;
+    long h = 0;
+    
+    long td = long(d);
+    
+    while (1){
+        
+        if ((a * k) > td){
+            break;
+        }
+        hight = sqrt((td*td) - ((a*k) * (a*k)));
+        h = floor(hight);
+        
+        answer += floor(h/k) + 1;
+        
+        a++;
+    }
+    return answer;
+}
