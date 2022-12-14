dacae3faf54642dd23fdf04288343a9303dc83b9
hedwig3798 <71327618+hedwig3798@users.noreply.github.com>
2022-12-14 17:11:32 +0900

    Create solution.cpp

---
+++ programers/택배상자/solution.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(vector<int> order) {
+    int answer = 0;
+    int now = 0;
+    stack<int> s;
+    vector<int> truck;
+    
+    for (int i = 1; i <= order.size(); i++){
+        // printf("%d, %d\n", now, i);
+        if (i == order[now]){
+            now ++;
+            answer ++;
+        }
+        else {
+            s.push(i);
+        }
+        while(1){
+            if (s.empty()){
+                break;
+            }
+            if (s.top() == order[now]){
+                now ++;
+                answer ++;
+                s.pop();
+            }
+            else{
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
