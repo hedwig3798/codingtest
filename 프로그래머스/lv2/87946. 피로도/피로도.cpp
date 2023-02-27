1cc8f0422b1225393ee1fa2662f158de901edc68
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2023-02-27 14:03:02 +0900

    [level 2] Title: 피로도, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int answer = 0;
+
+void search(int k, vector<vector<int>> dungeons, vector<int> flag, int now){
+    //printf("%d, %d\n", k, now);
+    for (int i = 0 ; i < flag.size(); i ++){
+        
+        if (flag[i] == 0 && dungeons[i][0] <= k){
+            flag[i] = 1;
+            search(k-dungeons[i][1], dungeons, flag, now+1);
+            flag[i] = 0;
+        }      
+    }
+    if (answer < now){
+        answer = now;
+    }
+    return;
+}
+
+int solution(int k, vector<vector<int>> dungeons) {
+    vector<int>flag (dungeons.size(), 0);
+    search(k, dungeons, flag, 0);
+    return answer;
+}
