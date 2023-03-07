7217bf11a9670f30e8c510b49095ac3389bba559
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2023-03-07 16:12:50 +0900

    [unrated] Title: 덧칠하기, Time: 0.14 ms, Memory: 4.99 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 0;
+    
+    int end = 0;
+    for (auto i : section){
+        if (i < end)
+            continue;
+        answer++;
+        end = i + m;
+    }
+    
+    return answer;
+}
