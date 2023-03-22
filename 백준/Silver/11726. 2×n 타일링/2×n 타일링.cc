bdc78f4d8a65e0e4b8b269a754e90cf0739fb4c2
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2023-03-22 12:15:28 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	int n;
+	cin >> n;
+
+	vector<int> dp(1001, 0);
+
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i < 1001; i++) {
+		dp[i] += (dp[i - 1] + dp[i - 2]) % 10'007;
+	}
+
+	cout << dp[n];
+
+	return 0;
+} 
