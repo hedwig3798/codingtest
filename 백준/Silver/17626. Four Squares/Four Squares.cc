eae4bbdf8432a35e4c26896103ff50dc88093bdf
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2023-03-23 13:04:52 +0900

    [Silver III] Title: Four Squares, Time: 16 ms, Memory: 2276 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	int n;
+	cin >> n;
+
+	vector<int> dp(n + 1, INT_MAX/2);
+	dp[0] = 0;
+	dp[1] = 1;
+
+	for (int i = 2; i <= n ; i++) {
+		if ((int)sqrt(i) * (int)sqrt(i) == i) {
+			dp[i] = 1;
+			continue;
+		}
+
+		for (int j = 1; j*j < i; j++) {
+			dp[i] = min(dp[i], dp[j*j] + dp[i - (j*j)]);
+		}
+	}
+	cout << dp[n];
+	return 0;
+} 
