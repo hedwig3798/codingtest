79dd07fc4a5ffc0ca62592e8026e78dfbef6fcfe
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2023-04-10 11:08:09 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2936 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int n, k;
+	cin >> n >> k;
+	int limit = 100001;
+	vector<int> dp(limit*2, -1);
+	dp[n] = 0;
+	deque<int> q;
+	q.push_front(n);
+
+	for (;!q.empty();) {
+		int now = q.front();
+		q.pop_front();
+		if (now == k){
+			break;
+		}
+		if (now * 2 <= limit && dp[now * 2] == -1) {
+			dp[now * 2] = dp[now];
+			q.push_front(now * 2);
+		}
+		if (now + 1 <= limit && dp[now + 1] == -1) {
+			dp[now + 1] = dp[now] + 1;
+			q.push_back(now + 1);
+		}
+		if (now - 1 >= 0 && dp[now - 1] == -1) {
+			dp[now - 1] = dp[now] + 1;
+			q.push_back(now - 1);
+		}
+		
+	}
+	cout << dp[k];
+	return 0;
+}
