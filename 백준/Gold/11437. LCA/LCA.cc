953f8a839b23b2b109d42341b119cbcdef63f4b2
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2026-01-30 15:13:54 +0900

    [Gold III] Title: LCA, Time: 28 ms, Memory: 8428 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.cc
+++ 백준/Gold/11437. LCA/LCA.cc
@@ -3,12 +3,14 @@
 #include <unordered_map>
 
 #define MAX_COUNT 50'001
+#define LOG 17
 
 int N, M;
 std::vector<int> tree[MAX_COUNT];
 
 int level[MAX_COUNT] = { 0, };
-int parent[MAX_COUNT] = { 0, };
+int parent[MAX_COUNT][LOG] = { 0, };
+
 
 int main()
 {
@@ -26,7 +28,7 @@ int main()
 	}
 
 	level[1] = 1;
-	parent[1] = 0;
+	parent[1][0] = 0;
 	std::queue<int> q;
 	q.push(1);
 	while (false == q.empty())
@@ -34,19 +36,32 @@ int main()
 		int curr = q.front();
 		q.pop();
 
-		for(auto& next : tree[curr])
+		for (auto& next : tree[curr])
 		{
 			if (0 == level[next])
 			{
 				level[next] = level[curr] + 1;
-				parent[next] = curr;
+				parent[next][0] = curr;
 				q.push(next);
 			}
 		}
 	}
 
+	for (int i = 1; i < LOG; i++)
+	{
+		for (int j = 1; j <= N; j++)
+		{
+			if (0 != parent[j][i - 1])
+			{
+				parent[j][i] = parent[parent[j][i - 1]][i - 1];
+			}
+		}
+	}
+
+
+
 	std::cin >> M;
-	for (int i = 0; i < M ; i++)
+	for (int i = 0; i < M; i++)
 	{
 		int a, b;
 		std::cin >> a >> b;
@@ -56,21 +71,30 @@ int main()
 			std::swap(a, b);
 		}
 
-		int sub = level[a] - level[b];
-
-		while (0 < sub)
+		for (int k = LOG - 1; k >= 0 ; k--)
 		{
-			a = parent[a];
-			sub--;
+			if (level[a] - (1 << k) >= level[b])
+			{
+				a = parent[a][k];
+			}
 		}
 
-		while (a != b)
+		if (a == b)
 		{
-			a = parent[a];
-			b = parent[b];
+			std::cout << a << '\n';
+			continue;
 		}
 
-		std::cout << a << '\n';
+		for (int k = LOG - 1; k >= 0; k--) 
+		{
+			if (parent[a][k] != parent[b][k]) 
+			{
+				a = parent[a][k];
+				b = parent[b][k];
+			}
+		}
+
+		std::cout << parent[a][0] << '\n';
 	}
 
 	return 0;
